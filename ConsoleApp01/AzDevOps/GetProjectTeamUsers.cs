e37dd90e607051f48bfb964b346df62c2ce613b5
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-07-07 02:10:09 +0100

    Azure DevOps project team and members

---
+++ ConsoleApp01/AzDevOps/GetProjectTeamUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Using Microsoft.TeamFoundation.Core.WebApi,
+
+namespace ConsoleApp01.AzDevOps
+{
+    class GetProjectTeamUsers
+    {
+        public static void GetProjectTeamUser()
+        {
+            //using (TeamHttpClient teamHttpClient = new TeamHttpClient(uri, credentials))
+            //{
+            //    var teams = await teamHttpClient.GetTeamsAsync(azureDevOpsprojectId);
+            //    return teams;
+            //};
+            //and then get each member as:
+            //using (TeamHttpClient teamHttpClient = new TeamHttpClient(uri, credentials))
+            //{
+            //    var teamMembers = await teamHttpClient.GetTeamMembersWithExtendedPropertiesAsync(azureDevOpsprojectId, teamId);
+            //    return teamMembers;
+            //};
+        }
+    }
+}
